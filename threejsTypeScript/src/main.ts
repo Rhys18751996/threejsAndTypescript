52a11f9eccb556fe8d676ea1486affec58bd1cce
Rhys18751996 <18751996@student.westernsydney.edu.au>
2024-10-24 15:52:56 +1100

    up to 10. Camera

--- threejsTypeScript/src/main.ts
+++ threejsTypeScript/src/main.ts
@@ -7,7 +7,21 @@ import Stats from 'three/addons/libs/stats.module.js'
 //npm install @types/dat.gui@latest --save
 import { GUI } from 'dat.gui';
 
-const scene = new THREE.Scene()
+
+
+
+const sceneA = new THREE.Scene()
+sceneA.background = new THREE.Color(0x123456)
+
+const sceneB = new THREE.Scene()
+sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
+
+const sceneC = new THREE.Scene()
+sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+//sceneC.backgroundBlurriness = 0.5
+
+
+
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
@@ -28,16 +42,33 @@ const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+sceneA.add(cube)
 
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
+let activeScene = sceneA
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA
+  },
+  sceneB: () => {
+    activeScene = sceneB
+  },
+  sceneC: () => {
+    activeScene = sceneC
+  },
+}
+
 
 
 
 const gui = new GUI()
+const sceneFolder = gui.addFolder('Scene')
+sceneFolder.add(setScene, 'sceneA').name('Scene A')
+sceneFolder.add(setScene, 'sceneB').name('Scene B')
+sceneFolder.add(setScene, 'sceneC').name('Scene C')
 
 const cubeFolder = gui.addFolder('Cube')
 cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
@@ -60,7 +91,7 @@ function animate() {
   cube.rotation.x += 0.005
   cube.rotation.y += 0.005
 
-  renderer.render(scene, camera)
+  renderer.render(activeScene, camera)
 
   stats.update()
 }
