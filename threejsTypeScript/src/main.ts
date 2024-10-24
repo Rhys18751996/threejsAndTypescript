89792355c69429a2d2fb2732163456bdbe9dbe68
Rhys18751996 <18751996@student.westernsydney.edu.au>
2024-10-24 14:01:20 +1100

    up to install three.js course boilderPlate

--- threejsTypeScript/src/main.ts
+++ threejsTypeScript/src/main.ts
@@ -1,6 +1,11 @@
 import './style.css';
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
+
+//npm install dat.gui@latest --save
+//npm install @types/dat.gui@latest --save
+import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene()
 
@@ -25,6 +30,30 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+
+
+
+const gui = new GUI()
+
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.add(cube.scale, 'x', 0, 5)
+cubeFolder.open()
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 20)
+cameraFolder.open()
+
+
+
+
+
 function animate() {
   requestAnimationFrame(animate)
 
@@ -32,6 +61,8 @@ function animate() {
   cube.rotation.y += 0.005
 
   renderer.render(scene, camera)
+
+  stats.update()
 }
 
 animate()
